f346804f085bfbbcae9d3343b54f197e68fdd1d4
amirhossein-mohammadpoor <mohammadpoor104@gmail.com>
2020-08-05 14:46:43 +0430

    EndingSlide created

---
+++ src/components/slides/EndingSlide.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./EndingSlide.scss"
+
+const EndingSlide = props => {
+  return (
+    <div className="ending-slide">
+      <h3>.با تشکر از وقتی که صرف پاسخگویی به سوالات کردید</h3>
+      <img  src="/images/logo.png" alt="logo" className="logo d-none d-sm-block" />
+    </div>
+  )
+}
+
+export default EndingSlide
